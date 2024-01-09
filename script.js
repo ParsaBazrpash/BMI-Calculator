d08c7b1b182848ac114a156810c6cc73845dd5c2
Parsa Bazrpash Amalgar <148006143+ParsaBazrpash@users.noreply.github.com>
2024-01-08 18:07:53 -0600

    fix a bug

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function calculate() {
         document.getElementById("results").innerHTML = error;
         return;
     }
+    
 
     // Convert input to numbers
     weight = parseFloat(weightInput);
@@ -23,8 +24,9 @@ function calculate() {
     // Convert height to meters if it's in feet
     if (document.getElementById("heightUnit").value === "feet") {
         height *= 0.3048; // 1 foot = 0.3048 meters
+    } else if (document.getElementById("heightUnit").value === "cm") {
+        height /= 100; // 1 meter = 100 centimeters
     }
-
     // Convert weight to kilograms if it's in pounds
     if (document.getElementById("weightUnit").value === "pounds") {
         weight *= 0.453592; // 1 pound = 0.453592 kilograms
